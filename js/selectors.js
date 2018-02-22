4ad6d0fcbaed781c4d893732f52744e94a0cca23
Matt Zuzolo <mattzuzolo@outlook.com>
2018-02-22 01:33:25 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -7,4 +7,17 @@ function paragraphSelector () {
 
 function lastImageSelector() {
   
+
+}
+
+function ninjaBabySelector() {
+  
+}
+
+function divSelector(){
+  
+}
+
+function firstListItem(){
+  
 }
